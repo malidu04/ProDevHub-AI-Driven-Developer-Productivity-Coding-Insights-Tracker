79858d6d2216759b54b47d2652ab619bcd28f002
malidu04 <malidupahasara04@gmail.com>
2025-11-09 18:37:43 +0530

    loader and footer changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Loader from './components/Loader'
+
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
         <div className="min-h-screen bg-gray-50 flex flex-col">
           <Navbar />
           <main className="flex-1">
-            
+            <Loader />
           </main>
           <Footer />
         </div>
